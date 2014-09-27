902097f3af31275ca10ecc901719daa2f199a8c5
Christopher S. Case <chris.case@g33xnexus.com>
2014-09-27 18:36:54 -0500

    rewrote the socket wrapper to conform to the new socket api on the server.

--- src/services/socket.js
+++ src/services/socket.js
@@ -4,28 +4,54 @@
 // @module socket.js
 // ---------------------------------------------------------------------------------------------------------------------
 
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 var Promise = require('bluebird');
 
-function SocketService($timeout) {
+// ---------------------------------------------------------------------------------------------------------------------
+
+function SocketService($timeout)
+{
     this.$timeout = $timeout;
 } // end SocketService
 
-SocketService.prototype.connect = function (url) {
+util.inherits(SocketService, EventEmitter);
+
+SocketService.prototype.connect = function(url)
+{
     this.socket = io(url);
+
+    // We only have to worry about events from the server.
+    this.socket.on('event', this._handleEvent.bind(this));
 }; // end connect
 
-SocketService.prototype.request = function () {
-    var self = this;
-    var args = Array.prototype.slice.call(arguments, 0);
+SocketService.prototype.event = function()
+{
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift('event');
 
-    return new Promise(function (resolve, reject)
+    if(this.socket)
+    {
+        this.socket.emit.apply(this.socket, arguments);
+    } // end if
+};
+
+SocketService.prototype.request = function()
+{
+    var self = this;
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift('request');
+
+    return new Promise(function(resolve, reject)
         {
-            var wrappedCB = function () {
+            var wrappedCB = function()
+            {
                 var args = Array.prototype.slice.call(arguments, 0);
 
                 // By wrapping this in `$timeout(0)`, we schedule this to be run on the next digest cycle,
                 // and handle the need for `$apply`.
-                self.$timeout(function () {
+                self.$timeout(function()
+                {
                     resolve(args);
                 }, 0);
             };
@@ -34,46 +60,37 @@ SocketService.prototype.request = function () {
             args.push(wrappedCB);
 
             // Emit over socket.io
-            if (self.socket) {
+            if(self.socket)
+            {
                 self.socket.emit.apply(self.socket, args);
             } // end if
         }
     );
 }; // end emit
 
-SocketService.prototype.on = function (event, callback) {
-    var self = this;
-    var wrappedCB = function () {
-        var args = Array.prototype.slice.call(arguments, 0);
+SocketService.prototype._handleEvent = function()
+{
+    this.emit.apply(this, arguments);
+}; // end _handleEvent
 
-        // By wrapping this in `$timeout(0)`, we schedule this to be run on the next digest cycle,
-        // and handle the need for `$apply`.
-        self.$timeout(function () {
-            callback.apply(callback, args);
-        }, 0);
-    };
+/*
+ SocketService.prototype.on = function (event, callback) {
+ var self = this;
+ var wrappedCB = function () {
+ var args = Array.prototype.slice.call(arguments, 0);
 
-    if (this.socket) {
-        this.socket.on.apply(this.socket, [event, wrappedCB]);
-    } // end if
-}; // end on
+ // By wrapping this in `$timeout(0)`, we schedule this to be run on the next digest cycle,
+ // and handle the need for `$apply`.
+ self.$timeout(function () {
+ callback.apply(callback, args);
+ }, 0);
+ };
 
-SocketService.prototype.channel = function (channel) {
-    function SocketChannel() {
-        this.socket = io.connect(channel)
-    } // end SocketChannel
-
-    SocketChannel.prototype.on = this.on;
-    SocketChannel.prototype.emit = this.emit;
-
-    return Promise.resolve(new SocketChannel());
-}; // end channel
-
-SocketService.prototype.event = function () {
-    if (this.socket) {
-        this.socket.emit.apply(this.socket, arguments);
-    } // end if
-};
+ if (this.socket) {
+ this.socket.on.apply(this.socket, [event, wrappedCB]);
+ } // end if
+ }; // end on
+ */
 
 // ---------------------------------------------------------------------------------------------------------------------
 
