a8ee2a667fd39ec17df6bacafa3a4212184b86d1
David H. Bronke <dbronke@armt.com>
2014-10-21 23:21:06 -0500

    Load stuff, and render it!

--- src/widgets/game/game.js
+++ src/widgets/game/game.js
@@ -3,31 +3,77 @@
 //
 // @module game.js
 // ---------------------------------------------------------------------------------------------------------------------
+/* global angular: true */
 
-var _ = require('lodash');
-var Promise = require('bluebird');
-
-// ---------------------------------------------------------------------------------------------------------------------
-
-function GameCanvasFactory($window, Loader, keySvc)
+function GameCanvasFactory($window, $timeout, babylon, Loader, keySvc)
 {
-    function GameCanvasController()
+    var isSupported = babylon.Engine.isSupported();
+
+    function GameCanvasController($scope)
     {
+        $scope.isSupported = isSupported;
     } // end GameCanvasController
 
     // -----------------------------------------------------------------------------------------------------------------
 
-    function GameCanvasLink(scope, elem, attrs, controller)
+    function GameCanvasLink(scope, elem)//, attrs, controller
     {
+        var canvas, engine, loader;
+
+        if(isSupported)
+        {
+            console.log("Setting up canvas.");
+
+            canvas = elem.children('canvas');
+            console.log('canvas:', canvas);
+
+            $timeout(function()
+            {
+                engine = new babylon.Engine(canvas[0], true);
+
+                loader = new Loader(engine);
+
+                /*
+                loader.loadScene("scene.babylon")
+                    .then(function()
+                        {
+                            return loader.loadMesh("models/ares/ares.babylon");
+                        });
+                */
+                loader.loadMesh("models/ares/ares.babylon")
+                    .then(function()
+                        {
+                            return loader.createSkybox("models/stars/purplenebula_2048");
+                        })
+                //loader.createSkybox("models/stars/purplenebula_2048")
+                    .then(function()
+                        {
+                            console.log("Loading completed.");
+
+                            loader.startShit(canvas[0], function(camParams)
+                            {
+                                scope.$apply(function()
+                                {
+                                    scope.camera = camParams;
+                                });
+                            });
+                        },
+                        function(error)
+                        {
+                            console.error("Error loading:", error);
+                        });
+
+                // Listen for window resize events.
+                $window.addEventListener('resize', engine.resize.bind(engine));
+            });
+        } // end if
+
         // Setup the keybinding service
         keySvc.init(elem[0]);
 
         // Prepare the dom element.
         elem.addClass('game-canvas');
         //elem.append(controller.renderer.domElement);
-
-        // Listen for window resize events.
-        //$window.addEventListener('resize', controller.resize.bind(controller, elem));
 
         // Call resize to get the correct initial size.
         //setTimeout(controller.resize.bind(controller, elem), 250);
@@ -40,17 +86,22 @@ function GameCanvasFactory($window, Loader, keySvc)
 
     return {
         restrict: 'E',
+        scope: true,
         link: GameCanvasLink,
-        template: '<div id="game" contenteditable="true"></div>',
-        controller: GameCanvasController,
+        template: '<div id="game" contenteditable="true"><canvas></canvas>' +
+            '<div class="stats">Camera: alpha = {{ camera.alpha }}; beta = {{ camera.beta }}; radius = {{ camera.radius }}</div>' +
+            '<div class="error" ng-if="!isSupported">Your browser does not support WebGL! Go home!</div></div>',
+        controller: ['$scope', GameCanvasController],
         replace: true
-    }
+    };
 } // end GameCanvasFactory
 
 // ---------------------------------------------------------------------------------------------------------------------
 
 angular.module('rfi-client.widgets').directive('gameCanvas', [
     '$window',
+    '$timeout',
+    'babylon',
     'Loader',
     'KeyBindingService',
     GameCanvasFactory
