811423c1e87632cb6cfe3491996caaf239404576
Christopher S. Case <chris.case@g33xnexus.com>
2015-03-09 21:40:21 -0500

    fixed unit tests.

--- karma-config.js
+++ karma-config.js
@@ -9,12 +9,17 @@ module.exports = function(config)
             'dist/vendor/angular/angular.min.js',
             'dist/vendor/angular-mocks/angular-mocks.js',
             'dist/vendor/angular-route/angular-route.js',
-            'dist/vendor/babylonjs/babylon.1.14.js',
+            'dist/vendor/babylonjs/babylon.2.0.js',
             'dist/vendor/Keypress/keypress.js',
+            'dist/vendor/path/path.min.js',
+            'dist/vendor/eventemitter2/lib/eventemitter2.js',
+            'dist/vendor/bluebird/js/browser/bluebird.min.js',
+            'dist/vendor/lodash/lodash.min.js',
+            'dist/vendor/rfi-physics/dist/rfi-physics.js',
             'dist/vendor/socket.io-client/socket.io.js',
 
             // Compiled files to be tested
-            'dist/js/rfi-client.js',
+            'dist/js/rfi-client.min.js',
 
             // Tests
             'tests/**/*.spec.js'
