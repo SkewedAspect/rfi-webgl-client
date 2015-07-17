86b15c6923c6bf47c74e7d45a3745d5832d20c8a
Christopher S. Case <chris.case@g33xnexus.com>
2015-07-17 10:04:05 -0500

    big cleanup of code.

--- src/ui/chat/chat.js
+++ src/ui/chat/chat.js
@@ -4,16 +4,16 @@
 // @module chat.js
 // ---------------------------------------------------------------------------------------------------------------------
 
-function ChatWindowFactory(chatSvc, socketSvc)
+function ChatWindowFactory(_, chatSvc)
 {
     function ChatWindowController($scope)
     {
         $scope.showWindow = false;
-        $scope.state = {
-            activeRoom: 'general'
-        };
 
         Object.defineProperties($scope, {
+            activeRoom: {
+                get: function(){ return _.filter(this.rooms, { active: true })[0]; }
+            },
             rooms: {
                 get: function(){ return chatSvc.rooms; }
             }
@@ -26,58 +26,34 @@ function ChatWindowFactory(chatSvc, socketSvc)
             chatSvc.joinRoom('general')
                 .then(function()
                 {
-                    $scope.state['general'] = {
-                        history: [],
-                        message: "",
-                        unread: 0
-                    };
-
+                    $scope.setActive('general');
                     $scope.showWindow = true;
+
+                    // Join a second room
+                    return chatSvc.joinRoom('foobar');
                 });
-
-            // Join a second room
-            chatSvc.joinRoom('foobar')
-                .then(function()
-                {
-                    $scope.state['foobar'] = {
-                        history: [],
-                        message: "",
-                        unread: 0
-                    };
-                });
-        });
-
-        socketSvc.on('chat message', function(payload)
-        {
-            console.log('got message:', payload);
-
-            if(!$scope.state[payload.room])
-            {
-                $scope.state[payload.room] = { history: [] }
-            } // end if
-
-            $scope.state[payload.room].history.push(payload);
-
-            if(payload.room != $scope.state.activeRoom)
-            {
-                $scope.state[payload.room].unread = ($scope.state[payload.room].unread || 0) + 1;
-            } // end if
         });
 
         // -------------------------------------------------------------------------------------------------------------
 
-        $scope.setActive = function(room)
+        $scope.setActive = function(roomName)
         {
-            $scope.state.activeRoom = room;
-            $scope.state[room].unread = 0;
+            // Clear active rooms
+            _.each($scope.rooms, function(room){ room.active = false; });
+
+            // Set active room
+            var room = _.find($scope.rooms, { name: roomName });
+            (room || {}).active = true;
+            (room || {}).unread = 0;
         }; // end setActive
 
         $scope.send = function()
         {
-            chatSvc.sendMessage($scope.state.activeRoom, $scope.state[$scope.state.activeRoom].message)
+            //TODO: Parse messages to support commands
+            chatSvc.sendMessage($scope.activeRoom.name, $scope.activeRoom.message)
                 .then(function()
                 {
-                    $scope.state[$scope.state.activeRoom].message = "";
+                    $scope.activeRoom.message = "";
                 });
         }; // end send
     } // end ChatWindowController
@@ -93,8 +69,8 @@ function ChatWindowFactory(chatSvc, socketSvc)
 // ---------------------------------------------------------------------------------------------------------------------
 
 angular.module('rfi-client.widgets').directive('chatWindow', [
+    'lodash',
     'ChatService',
-    'SocketService',
     ChatWindowFactory
 ]);
 
